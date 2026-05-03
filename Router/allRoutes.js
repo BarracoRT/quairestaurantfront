a384dcbf40946afbc1a5293f9a44936660f329a2
Barraco <lprivera86@outlook.es>
2026-05-03 16:48:47 +0200

    ajout de la page la-carte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),];
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/la-carte", "La carte", "/pages/la-carte.html"),];
     
 
 //Le titre s'affiche comme ceci : Route.titre - websitename

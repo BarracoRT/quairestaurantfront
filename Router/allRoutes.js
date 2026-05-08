b929f57800479ec390eb1b647c57bc70eb526cbb
Barraco <lprivera86@outlook.es>
2026-05-08 13:49:22 +0200

    gestion de laficchage reservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changer de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos reservations", "/pages/reservations/allResa.html"),
 ];
     
 

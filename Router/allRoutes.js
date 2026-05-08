cb1c93eb87b17a17766e044d4965190008989921
Barraco <lprivera86@outlook.es>
2026-05-08 10:10:41 +0200

    Ajout inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,8 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/la-carte", "La carte", "/pages/la-carte.html"),
-    new Route("/signin", "Se connecter", "/pages/signin.html"),];
+    new Route("/signin", "Se connecter", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),];
     
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
